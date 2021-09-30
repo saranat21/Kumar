97a95ea792560dc4d39f47bfc66fa894759078e7
saranat21 <saranmuthuraju@gmail.com>
2021-09-30 17:53:25 +0530

    task 1 changed

--- src/com/tarining/Com/Holiday.java
+++ src/com/tarining/Com/Holiday.java
@@ -1,5 +1,12 @@
 package com.tarining.Com;
 
 public class Holiday {
-
+	public void diwali() {
+		System.out.println("pattasu");
+		
+	}
+public static void main(String[] args) {
+	Holiday H = new Holiday();
+	H.diwali();
+}
 }
